c1580ed6acb4577291fd3f19c501ed3686a2dca3
Chad <csebranek@gmail.com>
2022-09-08 09:52:43 -0500

    Add llod to frontend, attempt to look it up in assays table first

--- WNPRC_EHR/resources/web/wnprc_ehr/datasetButtons.js
+++ WNPRC_EHR/resources/web/wnprc_ehr/datasetButtons.js
@@ -1016,130 +1016,152 @@ WNPRC_EHR.DatasetButtons = new function(){
                 success: function (d) {
                     //check that all records are the same animal id
                     var toUpdate = d["rows"];
-                    new Ext4.Window({
-                        title: 'Change Request Status',
-                        width: 430,
-                        autoHeight: true,
-                        items: [{
-                            xtype: 'form',
-                            ref: 'theForm',
-                            bodyStyle: 'padding: 5px;',
-                            defaults: {
-                                border: false
-                            },
-                            items: [{
-                                html: 'Total Records: '+checked.length+'<br><br>',
-                                tag: 'div'
-                            },{
-                                xtype: 'combo',
-                                fieldLabel: 'Status',
-                                width: 300,
-                                triggerAction: 'all',
-                                mode: 'local',
-                                store: new LABKEY.ext4.Store({
-                                    xtype: 'labkey-store',
-                                    schemaName: 'lists',
-                                    queryName: 'status',
-                                    columns: 'Key,Status',
-                                    sort: 'Key',
-                                    //filterArray: [LABKEY.Filter.create('label', 'Request', LABKEY.Filter.Types.STARTS_WITH)],
-                                    autoLoad: true
-                                }),
-                                displayField: 'Status',
-                                valueField: 'Key',
-                                ref: 'qcstate',
-                                id: 'change-vl-qcstate',
-                                allowBlank: false
-                            }, {
-                                xtype: 'numberfield',
-                                ref: 'experiment',
-                                fieldLabel: 'Experiment #',
-                                width: 300,
-                                id: 'enter-experiment-number',
-                                allowBlank: false
-                            }, {
-                                xtype: 'numberfield',
-                                ref: 'positivecontrol',
-                                fieldLabel: 'Positive Control #',
-                                width: 300,
-                                id: 'enter-positive-control',
-                                allowBlank: false
-                            }, {
-                                xtype: 'textfield',
-                                ref: 'vlpositivecontrol',
-                                fieldLabel: 'Positive Control',
-                                width: 300,
-                                id: 'enter-vlpositive-control',
-                                allowBlank: false
-                            }, {
-                                xtype: 'textfield',
-                                ref: 'avgvlpositivecontrol',
-                                fieldLabel: 'AVG Positive Control',
-                                width: 300,
-                                id: 'enter-avgvlpositive-control',
-                                allowBlank: false
-                            }, {
-                                xtype: 'numberfield',
-                                ref: 'efficiency',
-                                fieldLabel: 'Efficiency',
-                                width: 300,
-                                id: 'efficiency',
-                                allowBlank: false,
-                                allowDecimals: true
-                            }
-                            ]
-                        }],
-                        buttons: [{
-                            text:'Submit',
-                            disabled:false,
-                            formBind: true,
-                            ref: '../submit',
-                            scope: this,
-                            handler: function(o) {
-                                var win = o.up('window');
-                                var form = win.down('form');
-                                var qc = form.getForm().findField('change-vl-qcstate').getValue();
-                                var num = form.getForm().findField('enter-experiment-number').getValue();
-                                var positive_control = form.getForm().findField('enter-positive-control').getValue();
-                                var vl_positive_control = form.getForm().findField('enter-vlpositive-control').getValue();
-                                var avg_vl_positive_control = form.getForm().findField('enter-avgvlpositive-control').getValue();
-                                var efficiency = form.getForm().findField('efficiency').getValue();
+                    var assay_llod;
+                    LABKEY.Query.selectRows({
+                        schemaName: 'Viral_Load_Assay',
+                        queryName: 'Assays',
+                        filterArray: [LABKEY.Filter.create('assayname', toUpdate[0]["assay"], LABKEY.Filter.Types.CONTAINS)],
+                        success: function (a) {
+                            assay_llod = a["rows"][0]["llod"];
+                            new Ext4.Window({
+                                title: 'Change Request Status',
+                                width: 430,
+                                autoHeight: true,
+                                items: [{
+                                    xtype: 'form',
+                                    ref: 'theForm',
+                                    bodyStyle: 'padding: 5px;',
+                                    defaults: {
+                                        border: false
+                                    },
+                                    items: [{
+                                        html: 'Total Records: '+checked.length+'<br><br>',
+                                        tag: 'div'
+                                    },{
+                                        xtype: 'combo',
+                                        fieldLabel: 'Status',
+                                        width: 300,
+                                        triggerAction: 'all',
+                                        mode: 'local',
+                                        store: new LABKEY.ext4.Store({
+                                            xtype: 'labkey-store',
+                                            schemaName: 'lists',
+                                            queryName: 'status',
+                                            columns: 'Key,Status',
+                                            sort: 'Key',
+                                            //filterArray: [LABKEY.Filter.create('label', 'Request', LABKEY.Filter.Types.STARTS_WITH)],
+                                            autoLoad: true
+                                        }),
+                                        displayField: 'Status',
+                                        valueField: 'Key',
+                                        ref: 'qcstate',
+                                        id: 'change-vl-qcstate',
+                                        allowBlank: false
+                                    }, {
+                                        xtype: 'numberfield',
+                                        ref: 'experiment',
+                                        fieldLabel: 'Experiment #',
+                                        width: 300,
+                                        id: 'enter-experiment-number',
+                                        allowBlank: false
+                                    }, {
+                                        xtype: 'numberfield',
+                                        ref: 'positivecontrol',
+                                        fieldLabel: 'Positive Control #',
+                                        width: 300,
+                                        id: 'enter-positive-control',
+                                        allowBlank: false
+                                    }, {
+                                        xtype: 'textfield',
+                                        ref: 'vlpositivecontrol',
+                                        fieldLabel: 'Positive Control',
+                                        width: 300,
+                                        id: 'enter-vlpositive-control',
+                                        allowBlank: false
+                                    }, {
+                                        xtype: 'textfield',
+                                        ref: 'avgvlpositivecontrol',
+                                        fieldLabel: 'AVG Positive Control',
+                                        width: 300,
+                                        id: 'enter-avgvlpositive-control',
+                                        allowBlank: false
+                                    }, {
+                                        xtype: 'numberfield',
+                                        ref: 'efficiency',
+                                        fieldLabel: 'Efficiency',
+                                        width: 300,
+                                        id: 'efficiency',
+                                        allowBlank: false,
+                                        allowDecimals: true
+                                    },{
+                                        xtype: 'textfield',
+                                        ref: 'assayllod',
+                                        fieldLabel: 'LLoD',
+                                        width: 300,
+                                        id: 'enter-llod',
+                                        allowBlank: false
+                                    }
+                                    ]
+                                }],
+                                buttons: [{
+                                    text:'Submit',
+                                    disabled:false,
+                                    formBind: true,
+                                    ref: '../submit',
+                                    scope: this,
+                                    handler: function(o) {
+                                        var win = o.up('window');
+                                        var form = win.down('form');
+                                        var qc = form.getForm().findField('change-vl-qcstate').getValue();
+                                        var num = form.getForm().findField('enter-experiment-number').getValue();
+                                        var positive_control = form.getForm().findField('enter-positive-control').getValue();
+                                        var vl_positive_control = form.getForm().findField('enter-vlpositive-control').getValue();
+                                        var avg_vl_positive_control = form.getForm().findField('enter-avgvlpositive-control').getValue();
+                                        var efficiency = form.getForm().findField('efficiency').getValue();
+                                        var assay_llod = form.getForm().findField('enter-llod').getValue();
 
-                                Ext4.Msg.wait('Loading...');
+                                        Ext4.Msg.wait('Loading...');
 
-                                //update qc status
-                                Ext4.each(toUpdate, function (row) {
-                                    row.Status = qc;
-                                    row.experimentNumber = parseInt(num);
-                                    row.positive_control = parseInt(positive_control);
-                                    row.vl_positive_control = vl_positive_control;
-                                    row.avg_vl_positive_control = avg_vl_positive_control;
-                                    row.efficiency = parseFloat(efficiency);
-                                }, this);
+                                        //update qc status
+                                        Ext4.each(toUpdate, function (row) {
+                                            row.Status = qc;
+                                            row.experimentNumber = parseInt(num);
+                                            row.positive_control = parseInt(positive_control);
+                                            row.vl_positive_control = vl_positive_control;
+                                            row.avg_vl_positive_control = avg_vl_positive_control;
+                                            row.efficiency = parseFloat(efficiency);
+                                            row.llod = assay_llod;
+                                        }, this);
 
-                                if (toUpdate.length) {
-                                    LABKEY.Query.updateRows({
-                                        schemaName: 'lists',
-                                        queryName: 'vl_sample_queue',
-                                        rows: toUpdate,
-                                        scope: this,
-                                        success: function () {
-                                            dataRegion.selectNone();
-                                            dataRegion.refresh();
-                                            Ext4.Msg.hide();
-                                        },
-                                        failure: EHR.Utils.onError
-                                    });
+                                        if (toUpdate.length) {
+                                            LABKEY.Query.updateRows({
+                                                schemaName: 'lists',
+                                                queryName: 'vl_sample_queue',
+                                                rows: toUpdate,
+                                                scope: this,
+                                                success: function () {
+                                                    dataRegion.selectNone();
+                                                    dataRegion.refresh();
+                                                    Ext4.Msg.hide();
+                                                },
+                                                failure: EHR.Utils.onError
+                                            });
 
-                                }
-                            }
-                        },{
-                            text: 'Close',
-                            handler: function(o){
-                                o.ownerCt.ownerCt.close();
-                            }
-                        }]
-                    }).show();
+                                        }
+                                    }
+                                },{
+                                    text: 'Close',
+                                    handler: function(o){
+                                        o.ownerCt.ownerCt.close();
+                                    }
+                                }]
+                            }).show(false, function() {
+                                document.getElementById('enter-llod-inputEl').value = assay_llod
+                            }, this);
+
+                        },
+                        failure: EHR.Utils.onError
+                    })
 
 
                 },
